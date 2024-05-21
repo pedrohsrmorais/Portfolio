bb983a0b4e01ace3d877362fdd4a49edbbc98422
Pedro Henrique <pedrinhu.moraes@hotmail.com>
2024-05-21 20:34:34 -0300

    build 0.1

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,22 +1,40 @@
 import linkedin from '../imgs/logos/LinkedIn_logo.png';
 import github from '../imgs/logos/GitHub-logo.png';
+import outlook from '../imgs/logos/Outlook_logo.png';
 
 export default function Contact({ language }) {
+    const copyToClipboard = (text) => {
+
+        navigator.clipboard.writeText(text).then(() => {
+
+            if (language === 'english') {
+                alert('text copied!');
+            } else {
+                alert('texto copiado com sucesso');
+            }
+        }).catch(err => {
+
+            console.error('Erro ao copiar texto: ', err);
+        });
+    };
 
     return (
         <div className="contact" id='contact'>
-            {language === 'english'?
-            <h2>Contact me right now via LinkedIn or email!</h2>
-            :
-            <h2>Me contate agora mesmo por linkedin ou email!</h2>
+            {language === 'english' ?
+                <h2>Contact me right now via LinkedIn or email!</h2>
+                :
+                <h2>Me contate agora mesmo por linkedin ou email!</h2>
             }
-            
 
             <img src={linkedin} className='imagesLogos' alt='linkedinLogo' onClick={
                 () => { window.open("https://www.linkedin.com/in/pedro-henrique-morais-56008b204/", "_blank") }} />
 
             <img src={github} className='imagesLogos' alt='linkedinLogo' onClick={
                 () => { window.open("https://github.com/pedrohsrmorais", "_blank") }
+            } />
+
+            <img src={outlook} className='imagesLogos' alt='outlookLogo' onClick={
+                () => { copyToClipboard('pedrinhu.moraes@hotmail.com') }
             } />
 
         </div>
